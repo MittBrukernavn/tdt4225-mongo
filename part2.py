ab0cc876a0a2b1acea21645e95c991e6f49f9c04
Aleksander Karlsson <aleksander.mats.karlsson@gmail.com>
2020-10-21 13:11:13 +0200

    Task 2 done + added some structure

--- part2.py
+++ part2.py
@@ -17,6 +17,19 @@ class Part2:
         trackpoint_count = self.trackpointCollection.count_documents({})
         print("User count:", user_count, "| Activity count:", activity_count, "| Trackpoint count:", trackpoint_count)
 
+    def task2(self):
+        # Find the average number of activities per user
+        user_count = self.userCollection.count_documents({})
+        activity_count = self.activityCollection.count_documents({})
+        avg = activity_count/user_count
+
+        # TODO: do they want avg activity per unique user?
+
+        # avg = self.activityCollection.aggregate([{"$group" : {"_id": "$user_id", "num_activities": {"$sum": 1}}}])
+        # below does that in mongodb console 
+        # db.Activity.aggregate([{$group: {_id: "$user_id", num_activities: {$sum: 1}}}])
+        print("Average activities per users: " + str(avg))
+
     def task4(self):
         # finds unique user ids in activities where transporation_mode is 'taxi'
         for user_id in self.activityCollection.find({'transportation_mode': 'taxi'}, {'user_id': 1, '_id': 0}).distinct('user_id'):
@@ -54,11 +67,14 @@ def main():
         print("\nQuery 1:\n")
         program.task1()
 
+        print("\nQuery 2:\n")
+        program.task2()
+
         print("\nQuery 4:\n")
-        program.task4()
+        # program.task4()
 
         print("\nQuery 5:\n")
-        program.task5()
+        # program.task5()
 
     except Exception as e:
         print("ERROR:", e)
